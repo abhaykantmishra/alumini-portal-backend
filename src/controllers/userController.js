cea875bbf2135271c29c1ab879bbd206f66c6805
abhaykantmishra <abhaykantmishra004@gmail.com>
2024-08-23 23:44:43 +0530

    j

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -49,7 +49,7 @@ async function loginUser(req,res){
             })
         }
 
-        const user = await User.findOne({email:email}).select(-password);
+        const user = await User.findOne({email:email}).select("-password");
 
         if(!user){
             return res.status(404).json({
