29d56e319d08c509e98945cc12871c32a72d3766
abhaykantmishra <abhaykantmishra004@gmail.com>
2024-08-23 17:30:20 +0530

    j

--- src/api/user/login.js
+++ src/api/user/login.js
@@ -1,5 +1,10 @@
 const {loginUser} = require('../../controllers/userController.js')
 
 module.exports = (req,res) => {
-    return loginUser(req,res)
+    if(req.method == 'post'){
+        return loginUser(req,res);
+    }
+    else {
+        return res.status(300).json({msg:"only post req!!"})
+    }
 }
