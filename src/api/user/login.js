bbc2ee432c27f8efa1d089fae0354a177c601f16
abhaykantmishra <abhaykantmishra004@gmail.com>
2024-08-23 17:40:31 +0530

    j

--- src/api/user/login.js
+++ src/api/user/login.js
@@ -3,7 +3,8 @@ const {loginUser} = require('../../controllers/userController.js')
 module.exports = (req,res) => {
 
     if(req.method === 'POST'){
-        return loginUser(req,res);
+        console.log("it is a post method");
+        loginUser(req,res);
     }
     else {
         return res.status(300).json({msg:"only post req!!"})
